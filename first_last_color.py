e84a4b82de09e3aaa89003105c0df58703d0a8bc
Faith Paule <125621727+faithmoselle@users.noreply.github.com>
2026-05-14 15:14:21 +0800

    Add first and last color display program

---
+++ first_last_color.py
@@ -0,0 +1,22 @@
+"""
+AUTHOR: Faith Moselle O. Paule
+DATE:
+Previous file name: LabAct2
+
+PROGRAM: First and Last Color Display
+LANGUAGE: Python 3
+TOPIC: Basic Python - List Indexing
+"""
+
+# Original color list
+color_list = ["Red", "Green", "White", "Black"]
+
+# Display original list
+print("Original List: Red, Green, White, Black")
+
+# Display first and last color using indices
+print("Displaying first and last color: ")
+
+# Index 0 = first element (Red)
+# Index 3 = fourth/last element (Black)
+print(color_list[0], color_list[3])
